ff360622861c07510849fa24b50c746de813fe65
Shivam Ratnam <shivam.ratnam@shiprocket.com>
2024-01-04 13:11:32 +0530

    package name fixed

--- Package.swift
+++ Package.swift
@@ -15,12 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WigzoNotificationContent",
-            dependencies: [],
-            exclude: [],
-            swiftSettings: [
-                .unsafeFlags(["-target", "x86_64-apple-ios11.0"]),
-            ]
+            name: "WigzoNotificationContent"
         ),
         .testTarget(
             name: "WigzoNotificationContentTests",
